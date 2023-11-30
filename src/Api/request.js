a0b7bfe92efa55005e58f1d3cab92b6ab2921fcf
Priyanga <uthirapriyanga@gmail.com>
2023-11-30 10:52:31 +0530

    adding banner

--- src/Api/request.js
+++ src/Api/request.js
@@ -1,6 +1,6 @@
 const API_KEY="823994791b713349df725dd8faca2bf2";
 
-export default {
+const requests={
   fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
   fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
   fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
@@ -9,5 +9,5 @@ export default {
   fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
   fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
   fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
-
 }
+export default requests
--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react'
+import "./Banner.css"
 import movieApi from "../../Api/movieApi"
 import requests from "../../Api/request"
 const Banner = () => {
@@ -6,6 +7,7 @@ const Banner = () => {
 
   useEffect(()=>{
     const fetchData=async()=>{
+
       try{
         const response =await movieApi.get(requests.fetchNetflixOriginals);
         setMovie(response.data.results[
@@ -21,17 +23,19 @@ const Banner = () => {
        
     }
     fetchData();
+    // return ()=>fetchData();
   },[])
-
-  
+console.log(movie);
+function truncate (string,n){
+  return string?.length>n?string.substr(0,n-1)+"...." :string;
+}
 
   return (
     <header className='banner'
     style={{
       backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie?.backdrop_path}")`,
-      backgroundSize: "cover",
       backgroundSize:"cover",
-      backgroundPosition: "center center",
+      backgroundPosition: "center center ",
     }}
     >
       <div className='banner-contents'>
@@ -42,7 +46,12 @@ const Banner = () => {
           <button className='banner-button'>Play</button>
           <button className='banner-button'>List</button>
         </div>
+        <h1 className="banner-description">
+          {truncate(movie?.overview,150)}
+        </h1>
       </div>
+
+      <div className='banner-fadeBottom'/>
 
     </header>
   )
