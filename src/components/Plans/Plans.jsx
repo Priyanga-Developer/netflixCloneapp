594bce7582a8fa8396ba95688b8ba91117848e77
Priyanga <uthirapriyanga@gmail.com>
2023-12-02 14:43:15 +0530

    adding profile details

--- src/components/Plans/Plans.jsx
+++ src/components/Plans/Plans.jsx
@@ -2,15 +2,54 @@ import React from 'react'
 import "./Plans.css"
 
 const Plans = () => {
+
+
+  const productDetails=[
+    { 
+      id:1,
+      plans:"Mobile",
+      description:"480p",
+      isSubscribe:true
+    },
+    {
+      id:2,
+      plans:"Basic",
+      description:"720p",
+      isSubscribe:false
+    },
+    {
+      id:3,
+      plans:"Standard",
+      description:"1080p",
+      isSubscribe:false
+    },
+    {
+      id:4,
+      plans:"Premium",
+      description:"4K+HDR",
+      isSubscribe:false
+    }]
+    const handleupdatePlans=(id)=>{
+      console.log(id)
+
+    }
+
   return (
-    <div>
-      <div className='plansScreenInfo'>
-        <h5>Product Name</h5>
-        <h6>Product Description</h6>
-      </div>
-      <button>Subscribe</button>
-    </div>
-  )
-}
+    <>
+    {productDetails.map((product)=>
+     <div className={`${
+      product.isSubscribe && "plansScreen__plan--disabled"
+    } plansScreen-plan`} key={product.id}>
+     <div className="plansScreen-info" >
+       <h5>{product.plans}</h5>
+       <h6>{product.description}</h6>
+     </div>
+      <button onClick={()=>handleupdatePlans(product.id)}>  {product.isSubscribe ? "Current Package" : "Subscribe"}</button>    
+   </div>
+    )}
+   
+  </>
+)
+  }
 
 export default Plans
